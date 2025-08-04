871651baf24597c7edfa9e893fcaff176393befb
rajeevansharan <rajeevansharan@gmail.com>
2025-08-04 17:09:18 +0530

    initialize the project

---
+++ src/main/java/com/BasicProject/PersonalContactBookApplication.java
@@ -0,0 +1,13 @@
+package com.BasicProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalContactBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalContactBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/BasicProject/PersonalContactBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BasicProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalContactBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
